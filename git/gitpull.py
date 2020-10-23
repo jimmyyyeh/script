da2a2859c1c0ccbec3504c2df1d1a821c18c47bc
Jimmy Yeh <chienfeng0719@gmail.com>
2020-10-23 10:15:20 +0800

    update gitpull.py script: allow pull multiple submodule

--- git/gitpull.py
+++ git/gitpull.py
@@ -8,23 +8,22 @@ class GitPull:
     _OPTS_DICT = None
     _BRANCH = 'develop'
     _MODULE_PATH = os.getcwd()
-    _SUBMODULE_PATH = None
-    _SUBMODULE_PATTERH = re.compile(r'\[submodule "(.+)"\]')
+    _SUBMODULE_PATH = list()
+    _SUBMODULE_PATTERN = re.compile(r'\[submodule "(.+)"\]')
 
     @classmethod
     def _get_submodule(cls):
         git_config_path = os.path.join(cls._MODULE_PATH, '.git/config')
         with open(git_config_path, 'r') as f:
             git_config = f.read()
-        submodule = cls._SUBMODULE_PATTERH.search(git_config).group(1) \
-            if cls._SUBMODULE_PATTERH.search(git_config) else None
-        if submodule:
-            cls._SUBMODULE_PATH = os.path.join(cls._MODULE_PATH, submodule)
+        if cls._SUBMODULE_PATTERN.search(git_config):
+            cls._SUBMODULE_PATH = [os.path.join(cls._MODULE_PATH, submodule)
+                                   for submodule in cls._SUBMODULE_PATTERN.findall(git_config)]
 
     @classmethod
     def run(cls):
         opts, args = getopt(sys.argv[1:], 'sb:', ['pullsubmodule=', 'branch='])
-        cls._OPTS_DICT = dict((opt) for opt in opts)
+        cls._OPTS_DICT = dict(opts)
         if '-b' in cls._OPTS_DICT:
             cls._BRANCH = cls._OPTS_DICT.get('-b')
 
@@ -36,9 +35,10 @@ class GitPull:
         if '-s' not in cls._OPTS_DICT or not cls._SUBMODULE_PATH:
             return
         os.system(f'git checkout {cls._BRANCH}')
-        os.chdir(cls._SUBMODULE_PATH)
-        os.system('git pull')
-        print('【GIT PULL SUBMODULE DONE.】')
+        for submodule_path in cls._SUBMODULE_PATH:
+            os.chdir(submodule_path)
+            os.system('git pull')
+            print(f'【PULL SUBMODULE {submodule_path} DONE.】')
 
 
 if __name__ == '__main__':
