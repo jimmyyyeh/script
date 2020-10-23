d93b7518a4f4f80cc661f50f91897e6eb196f918
Jimmy Yeh <chienfeng0719@gmail.com>
2020-10-23 13:04:51 +0800

    update gitpull.sh

--- git/gitpull.sh
+++ git/gitpull.sh
@@ -1,33 +1,39 @@
+pull_submodule(){
+
+  SUBMODULE_LOCATION=${CURRENT_LOCATION}/${SUBMODULE_PATH}
+  SUBMODULE_HEAD=$(cat ${CURRENT_LOCATION}/.git/modules/$SUBMODULE_PATH/HEAD)
+  SUBMODULE_BRANCH=$(echo $SUBMODULE_HEAD | awk -F'/' '{print $NF}')
+
+  cd $SUBMODULE_LOCATION
+  if [[ $SUBMODULE_BRANCH != ${DEV} ]]
+  then
+      git checkout $DEV
+  fi
+  echo "SUBMODULE: [${SUBMODULE_PATH}] BRANCH: [$SUBMODULE_BRANCH] -> [$DEV]"; git pull
+
+  cd $CURRENT_LOCATION
+}
+
 DEV="develop"
 CURRENT_LOCATION=$PWD
 PROJECT_NAME=$(basename $PWD)
 PROJECT_HEAD=$(cat $CURRENT_LOCATION/.git/HEAD)
 PROJECT_BRANCH=$(echo $PROJECT_HEAD | awk -F'/' '{print $NF}')
-SUBMODULE_PATH=$(git submodule | awk 'NR==1 {print $2}')
-# awk 'NR==1 {print $2}' limitation while the project has more than one submodules
-BRANCH=${1:-$PROJECT_NAME}
-SUBMODULE_NAME=$(echo $SUBMODULE_PATH | awk -F'/' '{print $NF}')
+SUBMODULE_PATH_STR=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+read -a SUBMODULE_PATH_LIST <<< $SUBMODULE_PATH_STR
 
+### awk 'NR==1 {print $2}' limitation while the project has more than one submodules
+#BRANCH=${1:-$PROJECT_NAME}
+#SUBMODULE_NAME=$(echo $SUBMODULE_PATH | awk -F'/' '{print $NF}')
+
+# pull current module
 if [[ $PROJECT_BRANCH != $DEV ]]
 then
     git checkout $DEV
 fi
 echo "PROJECT: [$PROJECT_NAME] | BRANCH: [$PROJECT_BRANCH] -> [$DEV]"; git pull
 
-if [ -z "$SUBMODULE_PATH" ]
-then
-    SUBMODULE_HEAD=''
-    SUBMODLUE_BRANCH=''
-else
-    SUBMODULE_HEAD=$(cat $CURRENT_LOCATION/.git/modules/$SUBMODULE_PATH/HEAD)
-    SUBMODULE_BRANCH=$(echo $SUBMODULE_HEAD | awk -F'/' '{print $NF}')
-
-    cd "$CURRENT_LOCATION/$SUBMODULE_PATH"
-    if [[ $SUBMODULE_BRANCH != $DEV ]]
-    then
-        git checkout $DEV
-    fi
-    echo "SUBMODULE: [$SUBMODULE_NAME] BRANCH: [$SUBMODULE_BRANCH] -> [$DEV]"; git pull
-fi
-
-cd $CURRENT_LOCATION
+# pull submodule
+for SUBMODULE_PATH in "${SUBMODULE_PATH_LIST[@]}"; do
+    pull_submodule
+done
