c9389aea226d46cca3d6f332ad171f29a931a70a
Jimmy Yeh <chienfeng0719@gmail.com>
2020-10-26 13:29:13 +0800

    update gitpull.py

--- git/gitpull.py
+++ git/gitpull.py
@@ -23,36 +23,28 @@ from getopt import getopt
 
 class GitPull:
     _OPTS_DICT = None
-    _BRANCH = 'develop'
     _MODULE_PATH = os.getcwd()
     _SUBMODULE_PATH = list()
-    _SUBMODULE_PATTERN = re.compile(r'\[submodule "(.+)"\]')
 
     @classmethod
     def _get_submodule(cls):
-        git_config_path = os.path.join(cls._MODULE_PATH, '.git/config')
-        if not os.path.isfile(git_config_path):
-            sys.exit("【GIT MODULE NOT FOUND.】")
-        with open(git_config_path, 'r') as f:
-            git_config = f.read()
-        if cls._SUBMODULE_PATTERN.search(git_config):
-            cls._SUBMODULE_PATH = [os.path.join(cls._MODULE_PATH, submodule)
-                                   for submodule in cls._SUBMODULE_PATTERN.findall(git_config)]
+        command = "git config --file .gitmodules --get-regexp path | awk '{ print $2 }'"
+        submodule_list = os.popen(command).readlines()
+        cls._SUBMODULE_PATH = [os.path.join(cls._MODULE_PATH, submodule.strip()) for submodule in submodule_list]
 
     @classmethod
     def run(cls):
         opts, args = getopt(sys.argv[1:], 'sb:', ['pullsubmodule=', 'branch='])
         cls._OPTS_DICT = dict(opts)
-        if '-b' in cls._OPTS_DICT:
-            cls._BRANCH = cls._OPTS_DICT.get('-b')
+        cls._BRANCH = cls._OPTS_DICT.get('-b', 'develop')
 
-        cls._get_submodule()
         os.system(f'git checkout {cls._BRANCH}')
         os.system('git pull')
         print('【GIT PULL MODULE DONE.】')
 
-        if '-s' not in cls._OPTS_DICT or not cls._SUBMODULE_PATH:
+        if '-s' not in cls._OPTS_DICT:
             return
+        cls._get_submodule()
         os.system(f'git checkout {cls._BRANCH}')
         for submodule_path in cls._SUBMODULE_PATH:
             os.chdir(submodule_path)
