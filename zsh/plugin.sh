4a35975b2ed0d4310e0d25d161b35c3ba65c16e2
Jimmy Yeh <chienfeng0719@gmail.com>
2020-11-11 10:14:41 +0800

    add zsh init plugin script

---
+++ zsh/plugin.sh
@@ -0,0 +1,8 @@
+# autojump
+brew install autojump
+# autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+# syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
