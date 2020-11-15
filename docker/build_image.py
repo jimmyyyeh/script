5aa516dde6ccc834400c9c574d3b36b90b8d7f6a
Jimmy Yeh <chienfeng0719@gmail.com>
2020-11-15 16:01:33 +0800

    update build_image.py

--- docker/build_image.py
+++ docker/build_image.py
@@ -14,21 +14,39 @@
         ┗┻┛    ┗┻┛
     God Bless,Never Bug
 """
+
 import os
 import sys
+from getopt import getopt
 
 
 class BuildImage:
+
     _REGISTRY_HOST = 'registry.feiwalk.com.tw'
 
     @classmethod
     def build(cls):
-        project_path = os.getcwd()
-        os.chdir(project_path)
-        project_name = os.path.basename(project_path)
-        tag = input("PLEASE INPUT YOUR TAG:")
-        print(f'BUILDING DOCKER IMAGE : {project_name} WITH TAG {tag}')
-        os.system(f'docker build -t {project_name} . --no-cache')
+        opts, args = getopt(sys.argv[1:], 'n:f:t:')
+        opts_dict = dict(opts)
+        if '-t' not in opts_dict:
+            sys.exit('missing tag arguments')
+        tag = opts_dict['-t']
+        if '-n' and '-f' in opts_dict:
+            project_name = opts_dict['-n']
+            docker_file = opts_dict['-f']
+        else:
+            project_path = os.getcwd()
+            os.chdir(project_path)
+            project_name = os.path.basename(project_path)
+            docker_file = None
+        print(f'BUILDING PROJECT: {project_name} DOCKER IMAGE: {docker_file} WITH TAG {tag}')
+        option = input('Y / N:')
+        if option.lower() != 'y':
+            sys.exit()
+        if docker_file:
+            os.system(f'docker build -t {project_name} -f {docker_file} . --no-cache')
+        else:
+            os.system(f'docker build -t {project_name} . --no-cache')
         os.system(f'docker tag {project_name} {cls._REGISTRY_HOST}/{project_name}:{tag}')
         os.system(f'docker push {cls._REGISTRY_HOST}/{project_name}:{tag}')
         os.system(f'docker tag {project_name} {cls._REGISTRY_HOST}/{project_name}:latest')
