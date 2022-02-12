5e0b3cbff27b645c3191fb186260a0e9df4562b9
Jimmy Yeh <chienfeng0719@gmail.com>
2022-02-12 13:24:04 +0800

    update k8s create role script

--- k8s/role/create_role.sh
+++ k8s/role/create_role.sh
@@ -1,41 +1,79 @@
-# sh ./create.sh $1=使用者名稱 $2=namespace $3=echo "APISERVER=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " ")"
+# sh ./create.sh $1=使用者名稱 $2=cluster name $3=role $4=namespace
+# APISERVER=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " ")
 
-openssl genrsa -out $1.key 2048 
-openssl req -new -key $1.key -out $1.csr -subj "/CN=$1"
+USERNAME=$1
+CLUSTER_NAME=$2
+ROLE=$3
+NAMESPACE=$4
+APISERVER=`kubectl config view -o jsonpath="{.clusters[*].cluster.server}"`
+KUBE_CONFIG=$USERNAME.kubeconfig
+KEY=$USERNAME.key
+CRT=$USERNAME.crt
+CSR=$USERNAME.csr
+
+## *** 如果下列檔案都產好了 可以只執行這一行就好 單純建立角色***
+#kubectl create rolebinding $USERNAME \
+#--clusterrole=$ROLE --user=$USERNAME --namespace=$NAMESPACE
+
+# 產生.key file
+if ! test -f "$KEY"; then
+    echo "========== GENERATE NEW $KEY =========="
+    openssl genrsa -out $USERNAME.key 2048
+fi
+
+# 產生.csr file
+if ! test -f "$CSR"; then
+    echo "========== GENERATE NEW $CSR =========="
+    openssl req -new -key $USERNAME.key -out $USERNAME.csr -subj "/CN=$USERNAME"
+fi
 
 cat <<EOF | kubectl create -f -
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
 metadata:
-  name: $1-csr
+  name: $USERNAME-csr
 spec:
   groups:
   - system:authenticated
-  request: $(cat $1.csr | base64 | tr -d '\n')
+  request: $(cat $USERNAME.csr | base64 | tr -d '\n')
   usages:
   - digital signature
   - key encipherment
   - client auth
 EOF
 
-kubectl certificate approve $1-csr
+kubectl certificate approve $USERNAME-csr
 
-kubectl get csr $1-csr -o jsonpath='{.status.certificate}' | base64 -D > $1.crt
+# 產生.crt file
+if ! test -f "$CRT"; then
+    echo "========== GENERATE NEW $CRT =========="
+    kubectl get csr $USERNAME-csr -o jsonpath='{.status.certificate}' | base64 -D > $USERNAME.crt
+fi
 
-kubectl create rolebinding $1 \
---clusterrole=read-pods --user=$1 --namespace=$2
+# 產生kubeconfig file
+if ! test -f "$KUBE_CONFIG"; then
+    echo "========== GENERATE NEW $KUBE_CONFIG =========="
+    kubectl config set-cluster $CLUSTER_NAME-cluster \
+      --insecure-skip-tls-verify=true \
+      --server=$APISERVER \
+      --kubeconfig=$USERNAME.kubeconfig
+fi
 
-kubectl config set-cluster $1-cluster \
-  --insecure-skip-tls-verify=true \
-  --server=$3 \
-  --kubeconfig=$1.kubeconfig
-kubectl config set-credentials $1 \
-  --client-certificate=$1.crt \
-  --client-key=$1.key \
+# 建立user
+kubectl config set-credentials $USERNAME \
+  --client-certificate=$USERNAME.crt \
+  --client-key=$USERNAME.key \
   --embed-certs=true \
-  --kubeconfig=$1.kubeconfig
-kubectl config set-context $1 \
-  --cluster=$1-cluster \
-  --user=$1 \
-  --namespace=$2 \
-  --kubeconfig=$1.kubeconfig
+  --kubeconfig=$USERNAME.kubeconfig
+# 建立context
+kubectl config set-context $CLUSTER_NAME-cluster \
+  --cluster=$CLUSTER_NAME-cluster \
+  --user=$USERNAME \
+  --namespace=$NAMESPACE \
+  --kubeconfig=$USERNAME.kubeconfig
+
+# 備份原本config檔案 避免遺失
+mv ~/.kube/config ~/.kube/config.bak
+# 覆蓋config檔案
+cp $USERNAME.kubeconfig ~/.kube/config
+kubectl config use-context $CLUSTER_NAME-cluster
